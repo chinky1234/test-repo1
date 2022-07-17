150854f37637a7050736a9eeefbf5bed70281467
dev3 <dev3@gmail.com>
2022-07-17 18:42:24 +0530

    from dev2

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
-class hello	#from dev1
+class hello	#from dev2
 {
-	public static void main()	#from dev1
+	public static void main()	#from dev2
 	{
-		system.out.println("hello modf from dev1");
+		system.out.println("hello modf to dev2");
 	}
-}
+}#from dev2
 
